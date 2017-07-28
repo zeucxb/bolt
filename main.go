b9ea346ba62d2d97a3dc735bf96f675b6ca34d34
Zeu Cxb <zeucxb@gmail.com>
2017-07-28 02:35:57 -0300

    First example, just to understand the concept

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"sync"
+)
+
+type asyncError struct {
+	RunnerName string
+	Err        error
+}
+
+var wg sync.WaitGroup
+
+func runner(name string, err chan<- asyncError, f interface{}, params ...interface{}) {
+	fValue := reflect.ValueOf(f)
+	fType := fValue.Type()
+
+	if fType.Kind() != reflect.Func {
+		err <- asyncError{name, errors.New("We need a valid function to work")}
+		return
+	}
+
+	r := make(chan error)
+	rValue := reflect.ValueOf(r)
+
+	in := []reflect.Value{rValue}
+
+	for _, param := range params {
+		pValue := reflect.ValueOf(param)
+		in = append(in, pValue)
+	}
+
+	wg.Add(2)
+	go fValue.Call(in)
+	go func() {
+		for {
+			select {
+			case e := <-r:
+				supervisor(asyncError{name, e})
+			}
+		}
+	}()
+	wg.Wait()
+}
+
+func supervisor(err asyncError) {
+	defer wg.Done()
+	fmt.Println(err.RunnerName)
+	fmt.Println(err.Err)
+}
+
+func main() {
+	err := make(chan asyncError)
+	runner("HARD TASK", err, hardTask, []int{1, 4, 6, 7})
+}
+
+func hardTask(err chan<- error, numbers []int) {
+	defer wg.Done()
+	fmt.Println(numbers)
+	err <- errors.New("EROOOOOR!!!")
+}
